2950492f17841127cd3e09d8ca9e96f5090a0273
Apoorva Kashyap <apoorvakashyap54@gmail.com>
2026-06-01 14:10:27 +0530

    fix(type-checks): Fixed some type annotations.

--- src/core_lens/__init__.py
+++ src/core_lens/__init__.py
@@ -1,4 +1,6 @@
 try:
-    from ._version import __version__
+    from ._version import __version__  # type: ignore[import-not-found,  attr-defined]
 except ImportError:
     __version__ = "0.1.0"
+
+_all__ = ["__version__"]
