d11ed8f16f353e88943d50f25ed2394818f6fc34
Apoorva Kashyap <apoorvakashyap54@gmail.com>
2026-06-08 17:16:39 +0530

    feat(entity): Added mws entity

--- src/core_lens/entities/__init__.py
+++ src/core_lens/entities/__init__.py
@@ -1,5 +1,6 @@
 __module__: str = "core_lens.entities"
 
 from core_lens.entities.tehsil import TehsilEntity
+from core_lens.entities.mws import MWSEntity
 
-__all__ = ["TehsilEntity"]
+__all__ = ["TehsilEntity", "MWSEntity"]
---
+++ src/core_lens/entities/mws.py
@@ -0,0 +1,31 @@
+from core_lens.base.entity import BaseEntity
+
+
+class MWSEntity(BaseEntity):
+    """Microwatershed (MWS) entity.
+
+    Backed by:
+    - Static: data/mws/static/mws.parquet
+    - Annual: data/mws/annual (partitioned by year)
+    - Fortnightly: data/mws/fortnightly (partitioned by basin then year)
+    """
+
+    @property
+    def key_cols(self) -> list[str]:
+        return ["mws_id"]
+
+    @property
+    def geometry_col(self) -> str:
+        return "geometry"
+
+    @property
+    def static_path(self) -> str:
+        return "data/mws/static"
+
+    @property
+    def annual_path(self) -> str | None:
+        return "data/mws/annual"
+
+    @property
+    def fortnightly_path(self) -> str | None:
+        return "data/mws/fortnightly"
